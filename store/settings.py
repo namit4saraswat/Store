c035978bdbf0eec4c0e0c8a86226848dfab03775
namit4saraswat <namit.and.saraswat@gmail.com>
2022-10-22 20:05:14 +0530

    config changes

--- store/settings.py
+++ store/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ea!sysjkv%3w*$@t_r@@p6)97z3-h9g5b3+71vuwmp%4+%+y2i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['statetender.herokuapp.com']
+ALLOWED_HOSTS = ['statetender.herokuapp.com', 'localhost']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://statetender.herokuapp.com'
